46d498741e8d04831fb080c814faf7c2e98c52f2
Thamsanqa <thamzafaniso@gmail.com>
2025-06-04 20:42:00 +0200

    Add pagination and filtering to MessageViewSet

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
 from .permissions import IsParticipant
+from .filters import MessageFilter
 
 class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
@@ -25,6 +26,7 @@ class MessageViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated, IsParticipant]
     filter_backends = [filters.SearchFilter]
     search_fields = ['message_body']
+    filterset_class = MessageFilter
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'chats',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -135,6 +136,10 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
+    
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20,
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
 
 # Custom user model
